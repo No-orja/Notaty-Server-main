eb8df6e39e595be57cc9389b622d9714b87b7c8d
No-orja <nomicro12311@gmail.com>
2025-04-08 11:12:40 +0300

    last update

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/notes', noteRoutes);
 app.use('/todos', todoRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
